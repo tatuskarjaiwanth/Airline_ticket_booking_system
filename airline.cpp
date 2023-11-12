aeec5445d600a8b7b614ef7cbce57aa1eec6d342
T JAIWANTH <jaiwanthtatuskar@gmail.com>
2023-11-12 13:54:46 +0530

    made proper validations

--- airline.cpp
+++ airline.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <queue>
 #include <array>
-
+#include <typeinfo>
 #include <fstream>
 #include <string>
 #include <vector>
 #include <sstream>
 #include <iomanip>
+#include <limits>
+#include <cctype>
 using namespace std;
 
 // Function to split a string into tokens based on a delimiter
@@ -67,53 +69,112 @@ queue<struct Passenger_Details> myQueue;
 
 
     
-int proper_age(int age)
-{
-    while (age < 0 || age > 100)
-    {
-        cout << "Enter the correct age: ";
-        cin >> age;
+
+int proper_age() {
+    int age;
+
+    while (true) {
+        cout << "Enter the age of the Passenger: ";
+
+        if (cin >> age) {
+            if (age >= 0 && age <= 100) {
+                break; 
+            } else {
+                cout << "Age must be between 0 and 100. Try again." << endl;
+            }
+        } else {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            cout << "Invalid input. Please enter a valid age." << endl;
+        }
     }
+
     return age;
 }
 
-
-string contact_validator(string num)
-{
-    while (!(num.length() == 10))
-    {
-        cout << "Enter the contact number which has only 10-digits:";
-        cin >> num;
-    }
-    return num;
-}
-
-int fli_num_validation(int num)
-{
-
-    int flag = 0;
-    while (!flag)
-    {
-        array<int, 11> flightNumbers = {123445, 234567, 345678, 456789, 567890, 678901, 789012, 890123, 901234, 123456, 234567};
-        for (int i = 0; i < 10; i++)
-        {
-            if (num == flightNumbers[i])
-                return num;
+bool isNumeric(const string& str) {
+    for (char ch : str) {
+        if (!isdigit(ch)) {
+            return false;
         }
-        cout << "Enter the correct flight number:";
+    }
+    return true;
+}
+
+string contact_validator() {
+    string num;
+
+    while (true) {
+        cout << "Enter the contact number which has only 10 digits: ";
         cin >> num;
+
+        if (num.length() == 10 && isNumeric(num)) {
+            break;
+        } else {
+            cout << "Invalid input. Please enter a 10-digit numeric contact number." << endl;
+        }
+    }
+
+    return num;
+
+}
+
+int fli_num_validation() {
+    int num;
+    array<int, 11> flightNumbers = {123445, 234567, 345678, 456789, 567890, 678901, 789012, 890123, 901234, 123456, 234567};
+    
+    while (true) {
+        // Prompt the user to enter the flight number
+        cout << "Enter the flight number: ";
+
+        // Check if the input operation was successful
+        if (cin >> num) {
+            // Check if the flight number is valid
+            for (int i = 0; i < 10; i++) {
+                if (num == flightNumbers[i]) {
+                    return num; // Valid flight number, exit the function
+                }
+            }
+            cout << "Invalid flight number. Try again." << endl;
+        } else {
+            cout << "Invalid input. Please enter a valid flight number." << endl;
+            // Clear the input buffer
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
     }
 }
 
-char gender_validation(char s)
-{
-    while (!(s == 'M' || s == 'm' || s == 'F' || s == 'f'))
-    {
-        cout << "Enter the gender correctly:";
-        cin >> s;
+
+char gender_validation() {
+    char gender;
+
+    
+    while (true) {
+        cout << "Enter the gender (M/F): ";
+
+        
+        if (cin >> gender) {
+            
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            if (gender == 'M' || gender == 'm' || gender == 'F' || gender == 'f') {
+                break; 
+            } else {
+                cout << "Invalid gender. Please enter 'M' or 'F'. Try again." << endl;
+            }
+        } else {
+            cout << "Invalid input. Please enter a single character. Try again." << endl;
+            
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
     }
-    return s;
+
+    return gender;
 }
+
 array<int, 11> flightNumbers = {123445, 234567, 345678, 456789, 567890, 678901, 789012, 890123, 901234, 123456, 234567};
 array<int,11> array_cost={5000, 7500, 6000, 4500, 5500, 6500, 4800, 7100, 5900, 4200, 5300};
 
@@ -158,22 +219,17 @@ void Book_reservation()
         cin.ignore();
         getline(cin,passenger[i].name);
 
-        cout << "Age of the passenger: ";
-        cin >> passenger[i].age;
-        passenger[i].age = proper_age(passenger[i].age);
+        
+        passenger[i].age = proper_age();
 
-        cout << "Gender(M/F): ";
-        cin >> passenger[i].Gender;
-        passenger[i].Gender = gender_validation(passenger[i].Gender);
+        passenger[i].Gender = gender_validation();
 
-        cout << "Contact No: ";
-        cin >> passenger[i].Contact;
-        passenger[i].Contact = contact_validator(passenger[i].Contact);
+        
+        passenger[i].Contact = contact_validator();
 
         if(i==0){
-        cout << "Flight Number(From the above displayed List): ";
-        cin >> passenger[i].flight_number;
-        passenger[i].flight_number = fli_num_validation(passenger[i].flight_number);
+        
+        passenger[i].flight_number = fli_num_validation();
          x = passenger[i].flight_number;
         }
         cost+=bill(passenger[i].flight_number);
@@ -213,9 +269,8 @@ void Cancel_reservation()
     cin.ignore();
     getline(cin , name);
     int num;
-    cout<<"Enter flight Number you want to delete present in ticket: ";
-    cin>>num;
-    num = fli_num_validation(num);
+   
+    num = fli_num_validation();
     int x = comparison(name , num);
     if(x==1){
         cout<<"Reservation Cancellled successfully\n\n";
@@ -233,7 +288,7 @@ void Display_flight_details()
 {
     queue<Passenger_Details> tempQueue = myQueue; // Create a copy of the original queue
     if(myQueue.empty()){
-        cout<<"There are no tickets Booked to delete\n\n";
+        cout<<"There are no tickets Booked to display\n\n";
         return;
     }
 
@@ -265,46 +320,80 @@ void Exit(int &a)
     a = 0;
 }
 
-void operation_of_menu()
-{
+// void operation_of_menu()
+// {
+//     int option = 1;
+
+//     while (option)
+//     {
+//         cout << "Enter the number of a menu option: ";
+//         cin >> option;
+//         switch (option)
+//         {
+//         case 1:
+//             Book_reservation();
+//             break;
+//         case 2:
+//             Cancel_reservation();
+//             break;
+//         case 3:
+//             Display_flight_details();
+//             break;
+//         case 4:
+//         case 0:
+//             Exit(option);
+//             break;
+
+//         default:
+
+//             cout << "-----------------------------------------------" << endl;
+//             cout << "         Airplane Ticket Booking SYSTEM            " << endl;
+//             cout << "\n";
+
+//             cout << "Please pick an option from the menu below." << endl;
+//             cout << "1. Book reservation" << endl;
+//             cout << "2. Cancel reservation" << endl;
+//             cout << "3. Display flight details" << endl;
+//             cout << "4. Exit(4 or 0) " << endl;
+//             cout << "\n";
+//             cout << "-----------------------------------------------" << endl;
+//             cout << "Enter proper Number from Menu" << endl;
+
+//             break;
+//         }
+//     }
+// }
+
+
+void operation_of_menu() {
     int option = 1;
 
-    while (option)
-    {
+    while (option) {
         cout << "Enter the number of a menu option: ";
-        cin >> option;
-        switch (option)
-        {
-        case 1:
-            Book_reservation();
-            break;
-        case 2:
-            Cancel_reservation();
-            break;
-        case 3:
-            Display_flight_details();
-            break;
-        case 4:
-        case 0:
-            Exit(option);
-            break;
 
-        default:
-
-            cout << "-----------------------------------------------" << endl;
-            cout << "         Airplane Ticket Booking SYSTEM            " << endl;
-            cout << "\n";
-
-            cout << "Please pick an option from the menu below." << endl;
-            cout << "1. Book reservation" << endl;
-            cout << "2. Cancel reservation" << endl;
-            cout << "3. Display flight details" << endl;
-            cout << "4. Exit(4 or 0) " << endl;
-            cout << "\n";
-            cout << "-----------------------------------------------" << endl;
-            cout << "Enter proper Number from Menu" << endl;
-
-            break;
+        if (cin >> option) {
+            switch (option) {
+                case 1:
+                    Book_reservation();
+                    break;
+                case 2:
+                    Cancel_reservation();
+                    break;
+                case 3:
+                    Display_flight_details();
+                    break;
+                case 4:
+                case 0:
+                    Exit(option);
+                    break;
+                default:
+                    cout << "Invalid option. Please enter a valid option from the menu." << endl;
+                    break;
+            }
+        } else {
+            cout << "Invalid input. Please enter a valid option from the menu." << endl;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
         }
     }
 }
